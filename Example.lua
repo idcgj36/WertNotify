7da928cfcd5393fa7b2fe148299837f80bebef73
idcgj36 <223230129+idcgj36@users.noreply.github.com>
2025-11-18 15:15:57 +0700

    Update Example.lua

--- Example.lua
+++ Example.lua
@@ -1,7 +1,4 @@
--- Enable animation
 Getgenv().Animation = true
-
--- Load WertUi library
 local WertUi = loadstring(game:HttpGet("https://raw.githubusercontent.com/idcgj36/WertNotify/refs/heads/main/Obfuscate.lua"))()
 
 -- Notification 1
@@ -13,24 +10,23 @@ WertUi:Notify({
     Transparent = 0.2,    -- Background transparency
     Icon = "rbxassetid://6034509993",    -- Icon ID
     StrokeThickness = 2,     -- Border thickness
-    StrokeColor = ColorGradient.new(     -- Gradient border
-        Color3.fromHex("#FF0000"),     -- Start color
-        Color3.fromHex("#FFAA00")     -- End color
-        -- you can add more colors than that
+    Color = ColorSequence.new( 
+        Color3.fromHex("#BD08FF"), 
+        Color3.fromHex("#00FFFF")
     ),
 })
 
 -- Notification 2
 WertUi:Notify({
-    Title = "Second Notification",       
-    Description = "This is the second notification example.", 
-    Duration = 5,                         
-    Corner = 8,                            
-    Transparent = 0.2,                     
-    Icon = "rbxassetid://6034510000",     
-    StrokeThickness = 2,                   
-    StrokeColor = ColorGradient.new(       
-        Color3.fromHex("#00FF00"),        
-        Color3.fromHex("#00FFFF")         
+    Title = "Second Notification",
+    Description = "This is the Second notification example.",
+    Duration = 4,
+    Corner = 8,
+    Transparent = 0.2,
+    Icon = "rbxassetid://6034509993",
+    StrokeThickness = 2,
+    Color = ColorSequence.new( 
+        Color3.fromHex("#BD08FF"), 
+        Color3.fromHex("#00FFFF")
     ),
 })
